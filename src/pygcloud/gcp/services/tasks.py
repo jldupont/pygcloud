856adf34ed8217f75fb7cdec7660d07c5729ef69
jeanlou.dupont <jeanlou.dupont@gmail.com>
2024-07-26 19:48:28 +0000

    TaskQueues requires location really

--- src/pygcloud/gcp/services/tasks.py
+++ src/pygcloud/gcp/services/tasks.py
@@ -8,29 +8,38 @@ https://cloud.google.com/sdk/gcloud/reference/tasks
 
 from pygcloud.models import Params, GCPServiceUpdatable
 from pygcloud.gcp.models import TaskQueue
+from pygcloud.gcp.services.iam import IAMBindingCapableMixin
 
 
-class TasksQueues(GCPServiceUpdatable):
+class TasksQueues(GCPServiceUpdatable, IAMBindingCapableMixin):
 
     LISTING_CAPABLE = True
     LISTING_REQUIRES_LOCATION = True
     DEPENDS_ON_API = "cloudtasks.googleapis.com"
     REQUIRES_UPDATE_AFTER_CREATE = False
+    LISTING_REQUIRES_LOCATION = True
     SPEC_CLASS = TaskQueue
     GROUP = ["tasks", "queues"]
 
     def __init__(
-        self, name: str, params_create: Params = [], params_update: Params = []
+        self,
+        name: str,
+        location: str,
+        params_create: Params = [],
+        params_update: Params = []
     ):
         super().__init__(name=name, ns="queues")
         self._params_create = params_create
         self._params_update = params_update
+        self.location = location
 
     def params_describe(self):
-        return ["describe", self.name, "--format", "json"]
+        return ["describe", self.name, "--format", "json", "--location", self.location]
 
     def params_create(self):
-        return ["create", self.name, "--format", "json"] + self._params_create
+        return ["create", self.name, "--format", "json", "--location", self.location] \
+            + self._params_create
 
     def params_update(self):
-        return ["update", self.name, "--format", "json"] + self._params_update
+        return ["update", self.name, "--format", "json", "--location", self.location] \
+            + self._params_update
