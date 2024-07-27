b41211a786a6f25fc9b8dbc473550f1a1317c724
jeanlou.dupont <jeanlou.dupont@gmail.com>
2024-07-27 00:23:20 +0000

    0.0.83

--- src/pygcloud/gcp/services/projects.py
+++ src/pygcloud/gcp/services/projects.py
@@ -16,7 +16,7 @@ class ProjectIAMBindingService(GCPServiceSingletonImmutable):
 
     def __init__(self, binding: IAMBinding, project_id: str):
         assert isinstance(binding, IAMBinding)
-        super().__init__()
+        super().__init__(f"{binding.member}, role={binding.role}", ns="projet_binding")
         self._binding = binding
         self._project_id = project_id
 
